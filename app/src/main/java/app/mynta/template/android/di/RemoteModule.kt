cf4ea0b3c966fa27cfd1bd72b61e8d42b486165f
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-08-27 05:18:41 +0300

    Add api-service to dagger-hilt.

--- app/src/main/java/app/mynta/template/android/di/RemoteModule.kt
+++ app/src/main/java/app/mynta/template/android/di/RemoteModule.kt
@@ -1,11 +1,18 @@
 package app.mynta.template.android.di
 
+import app.mynta.template.android.data.source.remote.APIService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RemoteModule {
-
+    @Provides
+    @Singleton
+    fun provideApi(): APIService {
+        return APIService.create()
+    }
 }
