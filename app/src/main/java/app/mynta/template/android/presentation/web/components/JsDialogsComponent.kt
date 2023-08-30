956db1285f5696ea811d40a5b9c74821b0cac3a6
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-08-30 14:50:41 +0300

    Move JS dialogs composables into a single file.

---
+++ app/src/main/java/app/mynta/template/android/presentation/web/components/JsDialogsComponent.kt
@@ -0,0 +1,312 @@
+package app.mynta.template.android.presentation.web.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import app.mynta.template.android.R
+import app.mynta.template.android.core.components.TextFieldComponent
+import app.mynta.template.android.ui.theme.MyntaTemplateTheme
+
+@Composable
+fun AlertDialogComponent(
+    title: String,
+    message: String,
+    onDismiss: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(
+            dismissOnClickOutside = true,
+            dismissOnBackPress = true
+        ),
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.background,
+                        shape = MaterialTheme.shapes.large
+                    )
+                    .padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(
+                    modifier = Modifier.alpha(.7f),
+                    text = message,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
+                        .padding(0.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primary
+                    ),
+                    shape = MaterialTheme.shapes.medium,
+                    contentPadding = PaddingValues(0.dp),
+                    onClick = onDismiss
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.dismiss),
+                        modifier = Modifier.padding(horizontal = 15.dp),
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun ConfirmDialogComponent(
+    title: String,
+    message: String,
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onCancel,
+        properties = DialogProperties(
+            dismissOnClickOutside = true,
+            dismissOnBackPress = true
+        ),
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.background,
+                        shape = MaterialTheme.shapes.large
+                    )
+                    .padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(
+                    modifier = Modifier.alpha(.7f),
+                    text = message,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    OutlinedButton(
+                        modifier = Modifier.weight(1f).height(40.dp).padding(0.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent
+                        ),
+                        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                        shape = MaterialTheme.shapes.medium,
+                        contentPadding = PaddingValues(0.dp),
+                        onClick = onCancel
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.cancel),
+                            modifier = Modifier.padding(horizontal = 15.dp),
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(15.dp))
+                    Button(
+                        modifier = Modifier.weight(1f).height(40.dp).padding(0.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary
+                        ),
+                        shape = MaterialTheme.shapes.medium,
+                        contentPadding = PaddingValues(0.dp),
+                        onClick = onConfirm
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.confirm),
+                            modifier = Modifier.padding(horizontal = 15.dp),
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun PromptDialogComponent(
+    message: String,
+    defaultValue: String,
+    onCancel: () -> Unit,
+    onConfirm: (result: String) -> Unit
+) {
+    val promptValue = remember { mutableStateOf(defaultValue) }
+
+    Dialog(
+        onDismissRequest = onCancel,
+        properties = DialogProperties(
+            dismissOnClickOutside = true,
+            dismissOnBackPress = true
+        ),
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.background,
+                        shape = MaterialTheme.shapes.large
+                    )
+                    .padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    text = "Prompt!",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(
+                    modifier = Modifier.alpha(.7f),
+                    text = message,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                TextFieldComponent(defaultValue = defaultValue, onValueChange = {
+                    promptValue.value = it
+                })
+                Spacer(modifier = Modifier.height(15.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    OutlinedButton(
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(40.dp)
+                            .padding(0.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent
+                        ),
+                        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                        shape = MaterialTheme.shapes.medium,
+                        contentPadding = PaddingValues(0.dp),
+                        onClick = onCancel
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.cancel),
+                            modifier = Modifier.padding(horizontal = 15.dp),
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(15.dp))
+                    Button(
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(40.dp)
+                            .padding(0.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary
+                        ),
+                        shape = MaterialTheme.shapes.medium,
+                        contentPadding = PaddingValues(0.dp),
+                        onClick = { onConfirm(promptValue.value) }
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.confirm),
+                            modifier = Modifier.padding(horizontal = 15.dp),
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Composable
+@Preview
+fun AlertDialogPreview() {
+    MyntaTemplateTheme {
+        AlertDialogComponent(
+            title = "Dialog Preview",
+            message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            onDismiss = { }
+        )
+    }
+}
+
+@Composable
+@Preview
+fun ConfirmDialogPreview() {
+    MyntaTemplateTheme {
+        ConfirmDialogComponent(
+            title = "Dialog Preview",
+            message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            onCancel = { },
+            onConfirm = { }
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PromptDialogPreview() {
+    MyntaTemplateTheme {
+        PromptDialogComponent(
+            message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            defaultValue = "",
+            onCancel = { },
+            onConfirm = { }
+        )
+    }
+}
