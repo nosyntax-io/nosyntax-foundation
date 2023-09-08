c7279cb8a3cab48330f235a605fe877ad0a8c55c
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-09-08 12:39:49 +0300

    Removed font family from local configuration.

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -4,6 +4,5 @@ object AppConfig {
     const val APP_VERSION_NUMBER = "config.app.version_number"
     const val APP_VERSION_NAME = "config.app.version_name"
     const val APP_ACCESS_TOKEN = "config.app.access_token"
-    const val APP_FONT_FAMILY = "config.app.font_family"
     const val APP_ONESIGNAL_ID = "config.app.onesignal_app_id"
 }
