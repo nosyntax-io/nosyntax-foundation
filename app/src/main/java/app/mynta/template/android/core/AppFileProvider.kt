3de4a11477d5265d16582c8254a4db7bff444b45
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-09-13 22:41:36 +0300

    Added support for camera uploads in webkit.

---
+++ app/src/main/java/app/mynta/template/android/core/AppFileProvider.kt
@@ -0,0 +1,6 @@
+package app.mynta.template.android.core
+
+import androidx.core.content.FileProvider
+
+class AppFileProvider: FileProvider() {
+}
--- app/src/main/java/app/mynta/template/android/presentation/web/WebScreen.kt
+++ app/src/main/java/app/mynta/template/android/presentation/web/WebScreen.kt
@@ -1,6 +1,7 @@
 package app.mynta.template.android.presentation.web
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
@@ -8,13 +9,13 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.JsPromptResult
 import android.webkit.JsResult
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
-import android.webkit.WebChromeClient.FileChooserParams
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
@@ -35,6 +36,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.FileProvider
 import app.mynta.template.android.core.Constants
 import app.mynta.template.android.core.components.ChangeScreenOrientationComponent
 import app.mynta.template.android.core.components.NoConnectionComponent
@@ -54,6 +56,11 @@ import app.mynta.template.android.presentation.web.components.ConfirmDialogCompo
 import app.mynta.template.android.presentation.web.components.JsDialog
 import app.mynta.template.android.presentation.web.components.LoadingIndicator
 import app.mynta.template.android.presentation.web.components.PromptDialogComponent
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
@@ -274,22 +281,59 @@ fun chromeClient(
     onCustomViewHidden: () -> Unit
 ): WebKitChromeClient {
     var filePath by remember { mutableStateOf<ValueCallback<Array<Uri>>?>(null) }
+    var imagePath by remember { mutableStateOf<String?>(null) }
 
     val fileChooser = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult(),
         onResult = { result ->
-            filePath?.onReceiveValue(FileChooserParams.parseResult(result.resultCode, result.data))
+            var results: Array<Uri> = emptyArray()
+
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+
+                if (data != null) {
+                    results = arrayOf(Uri.parse(data.dataString))
+                } else {
+                    if (imagePath != null) {
+                        val file = File(Uri.parse(imagePath).path)
+                        if (file.length() > 0) {
+                            results = arrayOf(Uri.parse(imagePath))
+                        }
+                    }
+                }
+            }
+            filePath?.onReceiveValue(results)
             filePath = null
         }
     )
 
     val chromeClient = remember {
         object: WebKitChromeClient() {
+            @SuppressLint("QueryPermissionsNeeded")
             override fun onShowFileChooser(webView: WebView?, filePathCallback: ValueCallback<Array<Uri>>, fileChooserParams: FileChooserParams): Boolean {
                 if (filePath != null) {
                     filePath?.onReceiveValue(null)
                 }
                 filePath = filePathCallback
+
+                var cameraCapture: Intent? = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                if (cameraCapture?.resolveActivity(context.packageManager) != null) {
+                    var imageFile: File? = null
+                    try {
+                        imageFile = createImageFile(context)
+                        cameraCapture.putExtra("PhotoPath", imagePath)
+                    } catch (e: IOException) {
+                        Log.e("TAG", e.localizedMessage)
+                    }
+                    if (imageFile != null) {
+                        imagePath = "file:" + imageFile.absolutePath
+                        cameraCapture.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(
+                            context, "${context.packageName}.provider", imageFile)
+                        )
+                    } else {
+                        cameraCapture = null
+                    }
+                }
 
                 val contentSelectionIntent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
                     addCategory(Intent.CATEGORY_OPENABLE)
@@ -309,9 +353,17 @@ fun chromeClient(
                 )
                 contentSelectionIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
 
+                val intentArray: Array<Intent?>
+                intentArray = if (cameraCapture != null) {
+                    arrayOf(cameraCapture)
+                } else {
+                    arrayOfNulls(0)
+                }
+
                 val chooserIntent = Intent(Intent.ACTION_CHOOSER).apply {
                     putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
                     putExtra(Intent.EXTRA_TITLE, "Upload")
+                    putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
                 }
                 fileChooser.launch(chooserIntent)
 
@@ -350,4 +402,21 @@ fun chromeClient(
     }
 
     return chromeClient
+}
+
+private fun createImageFile(context: Context): File? {
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+    val imageFileName = "JPEG_$timeStamp"
+    val mediaStorageDir = context.cacheDir
+    if (!mediaStorageDir.exists()) {
+        if (!mediaStorageDir.mkdirs()) {
+            Log.d("", "Oops! Failed create " + "WebView" + " directory")
+            return null
+        }
+    }
+    return File.createTempFile(
+        imageFileName,
+        ".jpg",
+        mediaStorageDir
+    )
 }
