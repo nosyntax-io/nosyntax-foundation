35631574e9bb1527bcf7da758d1f4fca17d21fcb
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-11-06 08:34:29 +0200

    Implemented basic auth for the core api.

--- app/src/main/java/app/mynta/template/android/data/source/remote/CoreApi.kt
+++ app/src/main/java/app/mynta/template/android/data/source/remote/CoreApi.kt
@@ -3,6 +3,7 @@ package app.mynta.template.android.data.source.remote
 import app.mynta.template.android.BuildConfig
 import app.mynta.template.android.data.source.remote.dto.app_config.AppConfigDto
 import app.mynta.template.android.data.source.remote.factory.ApiServiceFactory
+import app.mynta.template.android.data.source.remote.factory.BasicAuthCredentials
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -15,12 +16,18 @@ interface CoreAPI {
     ): AppConfigDto
 
     companion object {
-        private const val BASE_URL = "https://api.mynta.app/v1.3beta/"
+        private const val BASE_URL = "https://api.mynta.app/v1.4beta/"
         private const val ACCESS_TOKEN = BuildConfig.SERVER_ACCESS_TOKEN
 
         fun getInstance(): CoreAPI {
-            return ApiServiceFactory(baseUrl = BASE_URL, timeout = 10)
-                .create(CoreAPI::class.java)
+            return ApiServiceFactory(
+                baseUrl = BASE_URL,
+                basicAuth = BasicAuthCredentials(
+                    username = "username",
+                    password = "password"
+                ),
+                timeout = 10
+            ).create(CoreAPI::class.java)
         }
     }
 }
--- app/src/main/java/app/mynta/template/android/data/source/remote/factory/ApiServiceFactory.kt
+++ app/src/main/java/app/mynta/template/android/data/source/remote/factory/ApiServiceFactory.kt
@@ -1,15 +1,29 @@
 package app.mynta.template.android.data.source.remote.factory
 
-import app.mynta.template.android.core.utility.ConnectivityInterceptor
+import app.mynta.template.android.core.Constants
+import app.mynta.template.android.core.utility.Connectivity
+import okhttp3.Credentials
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
-class ApiServiceFactory(private val baseUrl: String, private val timeout: Long) {
+data class BasicAuthCredentials(
+    val username: String,
+    val password: String
+)
+
+class ApiServiceFactory(
+    private val baseUrl: String,
+    private val basicAuth: BasicAuthCredentials? = null,
+    private val timeout: Long,
+) {
     private val okHttpClient by lazy {
         OkHttpClient.Builder()
-            .addInterceptor(ConnectivityInterceptor())
+            .addInterceptor(ApiInterceptor(basicAuth = basicAuth))
             .readTimeout(timeout, TimeUnit.SECONDS)
             .writeTimeout(timeout, TimeUnit.SECONDS)
             .build()
@@ -22,5 +36,21 @@ class ApiServiceFactory(private val baseUrl: String, private val timeout: Long) 
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(clazz)
+    }
+}
+
+class ApiInterceptor(private val basicAuth: BasicAuthCredentials?) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        if (!Connectivity.getInstance().isOnline()) {
+            throw IOException(Constants.INTERNET_CONNECTION_EXCEPTION)
+        } else {
+            val requestBuilder = chain.request().newBuilder()
+
+            basicAuth?.let { credentials ->
+                val authHeader = Credentials.basic(credentials.username, credentials.password)
+                requestBuilder.header("Authorization", authHeader)
+            }
+            return chain.proceed(requestBuilder.build())
+        }
     }
 }
