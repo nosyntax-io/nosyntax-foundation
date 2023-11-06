67e875ccfce36f7c9894eaede1acb9783be49702
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-11-06 12:48:35 +0200

    Improved auth handling in interceptor.
    Added support for multiple authentication methods, including basic and bearer.

--- app/src/main/java/app/mynta/template/android/data/source/remote/CoreApi.kt
+++ app/src/main/java/app/mynta/template/android/data/source/remote/CoreApi.kt
@@ -1,16 +1,12 @@
 package app.mynta.template.android.data.source.remote
 
 import app.mynta.template.android.BuildConfig
-import app.mynta.template.android.core.Constants
-import app.mynta.template.android.core.utility.Connectivity
 import app.mynta.template.android.data.source.remote.dto.app_config.AppConfigDto
 import app.mynta.template.android.data.source.remote.factory.ApiServiceFactory
-import okhttp3.Interceptor
-import okhttp3.Response
+import app.mynta.template.android.data.source.remote.factory.Auth
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
-import java.io.IOException
 
 interface CoreAPI {
     @FormUrlEncoded
@@ -21,24 +17,14 @@ interface CoreAPI {
 
     companion object {
         private const val BASE_URL = "https://api.mynta.app/v1.4beta/"
+        private const val AUTH_TOKEN = BuildConfig.SERVER_AUTH_TOKEN
         private const val ACCESS_TOKEN = BuildConfig.SERVER_ACCESS_TOKEN
 
         fun getInstance(): CoreAPI {
             return ApiServiceFactory(
                 baseUrl = BASE_URL,
-                timeout = 10,
-                interceptor = object: Interceptor {
-                    override fun intercept(chain: Interceptor.Chain): Response {
-                        if (!Connectivity.getInstance().isOnline()) {
-                            throw IOException(Constants.INTERNET_CONNECTION_EXCEPTION)
-                        } else {
-                            val requestBuilder = chain.request().newBuilder()
-                            requestBuilder.header("Authorization", "Bearer ${BuildConfig.SERVER_AUTH_TOKEN}")
-
-                            return chain.proceed(requestBuilder.build())
-                        }
-                    }
-                }
+                auth = Auth.Bearer(AUTH_TOKEN),
+                timeout = 10
             ).create(CoreAPI::class.java)
         }
     }
