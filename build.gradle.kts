53c98a32f057bdc414c325205933240837451a6d
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2024-01-12 03:22:53 +0200

    Updated app dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,6 @@ plugins {
 
 buildscript {
     dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
     }
 }
