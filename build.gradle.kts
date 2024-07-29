c0223acb927069ed3137389aad1cd4d4813ab5ae
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2024-07-29 07:27:24 +0300

    Filter updates to stable versions in version catalog.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import nl.littlerobots.vcu.plugin.versionCatalogUpdate
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
@@ -13,5 +16,22 @@ buildscript {
     dependencies {
         classpath(libs.hilt.agp)
         classpath(libs.snakeyaml)
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val regex = """^[0-9,.v-]+(-r)?$""".toRegex()
+    return !regex.matches(version)
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                if (isNonStable(candidate.version) && !isNonStable(currentVersion)) {
+                    reject("Release candidate")
+                }
+            }
+        }
     }
 }
